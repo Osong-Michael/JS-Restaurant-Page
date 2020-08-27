bfe724f6f9fb12d91cf0d2e62c267bfd8da82037
Osong Agberndifor <agberndifor7@gmail.com>
2020-08-27 11:39:27 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+              loader: "babel-loader"
+        }
+      },
+
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {minimize: true}
+          }
+        ]
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+  ]
+}
