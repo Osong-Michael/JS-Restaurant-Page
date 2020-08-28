55843b03f0d78002e0cb414fd39f2e6ab4352f97
Osong Agberndifor <agberndifor7@gmail.com>
2020-08-28 11:59:06 +0100

    Add single page tab functionality

--- src/index.js
+++ src/index.js
@@ -13,19 +13,35 @@ const div3 = document.createElement('div');
 div3.innerHTML = contact;
 
 function show(e) {
-  
+  if (e.id === 'tab2') {
+    const child = content.children[1];
+    content.removeChild(child);
+    content.appendChild(div2);
+  }
+
+  if (e.id === 'tab3') {
+    const child = content.children[1];
+    content.removeChild(child);
+    content.appendChild(div3);
+  }
+
+  if (e.id === 'tab1') {
+    const child = content.children[1];
+    content.removeChild(child);
+    content.appendChild(div1);
+  }
 }
 
 
 window.onload = () => {
   content.innerHTML = navBar;
-  content.appendChild(div3);
+  content.appendChild(div1);
 
   const tab1 = document.getElementById('tab1');
   const tab2 = document.getElementById('tab2');
   const tab3 = document.getElementById('tab3');
 
-  tab1.addEventListener('click', () => {show(tab1)});
-  tab2.addEventListener('click', () => {show(tab2)});
-  tab3.addEventListener('click', () => {show(tab3)});
+  tab1.addEventListener('click', () => { show(tab1); });
+  tab2.addEventListener('click', () => { show(tab2); });
+  tab3.addEventListener('click', () => { show(tab3); });
 };
